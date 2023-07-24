88edf3b9a84684a471ff132a42d58c3c12617cf2
Vinicius <viniciuspereiraa51@gmail.com>
2023-07-24 02:44:18 -0300

    Projeto nikel

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,15 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
+
 let data = {
   transactions: [],
 };
 
 document.getElementById("btn-logout").addEventListener("click", logout);
+document.getElementById("btn-transactions").addEventListener("click", () => {
+  window.location.href = "transactions.html";
+});
 
 //Adicionar Lançamento
 document.getElementById("transaction-form").addEventListener("submit", (e) => {
@@ -26,8 +30,9 @@ document.getElementById("transaction-form").addEventListener("submit", (e) => {
   saveData(data);
   e.target.reset();
   myModal.hide();
-
-  alert("Lançamento adicionado!");
+  getCashIn();
+  getCashOut();
+  getTotal();
 });
 
 checkLogged();
@@ -47,6 +52,10 @@ function checkLogged() {
   if (dataUser) {
     data = JSON.parse(dataUser);
   }
+
+  getCashIn();
+  getCashOut();
+  getTotal();
 }
 
 function logout() {
@@ -54,6 +63,99 @@ function logout() {
   localStorage.removeItem("session");
 
   window.location.href = "index.html";
+}
+
+function getCashIn() {
+  const transactions = data.transactions;
+
+  const cashIn = transactions.filter((item) => item.type === "1");
+
+  if (cashIn.length) {
+    let cashInHtml = ``;
+    let limit = 0;
+
+    if (cashIn.length > 5) {
+      limit = 5;
+    } else {
+      limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+      cashInHtml += `<div class="row mb-4">
+      <div class="col-12">
+        <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+        <div class="container p-0">
+          <div class="row">
+            <div class="col-12 col-md-8"><p>${
+              cashIn[index].description
+            }</p></div>
+            <div
+              class="col-12 col-md-3 d-flex justify-content-end"
+            >
+              ${cashIn[index].date}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    }
+
+    document.getElementById("cash-in-list").innerHTML = cashInHtml;
+  }
+}
+
+function getCashOut() {
+  const transactions = data.transactions;
+
+  const cashIn = transactions.filter((item) => item.type === "2");
+
+  if (cashIn.length) {
+    let cashOutHtml = ``;
+    let limit = 0;
+
+    if (cashIn.length > 5) {
+      limit = 5;
+    } else {
+      limit = cashIn.length;
+    }
+
+    for (let index = 0; index < limit; index++) {
+      cashOutHtml += `<div class="row mb-4">
+      <div class="col-12">
+        <h3 class="fs-2">R$ ${cashIn[index].value.toFixed(2)}</h3>
+        <div class="container p-0">
+          <div class="row">
+            <div class="col-12 col-md-8"><p>${
+              cashIn[index].description
+            }</p></div>
+            <div
+              class="col-12 col-md-3 d-flex justify-content-end"
+            >
+              ${cashIn[index].date}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+    }
+
+    document.getElementById("cash-out-list").innerHTML = cashOutHtml;
+  }
+}
+
+function getTotal() {
+  const transactions = data.transactions;
+  let total = 0;
+
+  transactions.forEach((item) => {
+    if (item.type === "1") {
+      total += item.value;
+    } else {
+      total -= item.value;
+    }
+  });
+
+  document.getElementById("total").innerHTML = `R$ ${total.toFixed(2)}`;
 }
 
 function saveData(data) {
